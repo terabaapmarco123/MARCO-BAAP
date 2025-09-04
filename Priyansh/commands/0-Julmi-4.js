2b7ea85fb766ec888ed52ce0d9f3556554376835
terabaapmarco123 <terabaapaaya3344@gmail.com>
2025-09-04 10:20:27 +0530

    Update 0-Julmi-4.js

--- Priyansh/commands/0-Julmi-4.js
+++ Priyansh/commands/0-Julmi-4.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "jani",
     version: "1.1.1",
 	hasPermssion: 0,
-	credits: "JULMI", 
+	credits: "PRINCE", 
 	description: "Just Respond",
 	commandCategory: "no prefix",
     cooldowns: 5, 
