b6589014faca67f2347ef5d33c5f38dfc9fbf6e5
terabaapmarco123 <terabaapaaya3344@gmail.com>
2025-09-04 11:41:07 +0530

    Update 0_julmiboss.js

--- Priyansh/commands/0_julmiboss.js
+++ Priyansh/commands/0_julmiboss.js
@@ -13,8 +13,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
   if(react.includes("Me julmi hu BSDK") ||
-     react.includes("Tera malik hu") || react.includes("Tera boss hu julmi") || react.includes("Tera papa hu") ||
-react.includes("Tera boss hu julmi jaat") ||
+     react.includes("Tera malik hu") || react.includes("Tera boss hu prince") || react.includes("Tera papa hu") ||
+react.includes("Tera boss hu prince beby") ||
 react.includes("Malik se bckchodi")) {
     var msg = {
         body: `【  𝐒0𝐑𝐑𝐘 𝐌𝐀𝐋𝐈𝐊 𝐀𝐁 𝐆𝐀𝐋𝐓𝐈 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑𝐔𝐍𝐆𝐀 𝐏𝐋𝐒 𝐀𝐊 𝐁𝐀𝐑 𝐌𝐀𝐅 𝐊𝐀𝐑 𝐃0______🤐😙😥】`,
